77c13c10592dacc189872e232366ae964ccc9490
jacobhoang565 <161080610+jacobhoang565@users.noreply.github.com>
2024-08-10 08:46:44 +0000

    improvements

--- obfuscate.sh
+++ obfuscate.sh
@@ -12,16 +12,33 @@ sudo make install
 cd ..
 
 mv main.sh shc
+mv main_generate.sh shc
 
 cd shc
 
 # compile
 shc -f main.sh -o binary
 
-mv binary ..
+mv binary main
+
+mv main ..
+
+shc -f main_generate.sh -o binary
+
+mv binary main_generate
+
+mv main_generate ..
 
 mv main.sh ..
+
+mv main_generate.sh ..
 
 cd ..
 
 sudo rm -r shc
+
+mkdir binaries
+
+mv main binaries
+
+mv main_generate binaries
