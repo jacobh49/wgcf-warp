84a5436cc1d169eac337fd0f051aa89e0b7c1912
jacobhoang565 <161080610+jacobhoang565@users.noreply.github.com>
2024-08-11 04:09:55 +0000

    added comments

--- main_generate.sh
+++ main_generate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# clone and build wgcf
 git clone https://github.com/ViRb3/wgcf
 
 cd wgcf
@@ -8,6 +9,7 @@ go build -o wgcf
 
 chmod +x wgcf
 
+# fetch list and randomly choose key
 wget -nv https://gist.githubusercontent.com/jacobhoang565/5fbba0eab6dfd967088b064e96341a65/raw/e3be5456368f4212c0a83707874335182b221e42/CIm9fYZ4Pfv8s8V3g5z6.txt
 
 file_path="CIm9fYZ4Pfv8s8V3g5z6.txt"
@@ -20,10 +22,12 @@ echo "License key: $new_license_key"
 
 rm -r CIm9fYZ4Pfv8s8V3g5z6.txt
 
+# register account generate profilr
 ./wgcf register
 
 ./wgcf generate
 
+# replace license key in wgcf-account.toml
 update_license_key() {
   local new_license_key=$1
   sed -i "s/^license_key = '.*'/license_key = '$new_license_key'/" "wgcf-account.toml"
@@ -32,12 +36,14 @@ update_license_key() {
 
 update_license_key "$new_license_key"
 
+# update profile
 ./wgcf update
 
 ./wgcf generate
 
 ./wgcf status
 
+# move files back to ditectory
 mv wgcf wgcf-copy
 
 mv wgcf-account.toml ..
